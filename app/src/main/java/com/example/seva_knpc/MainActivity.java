008c81c6366b2d4dd298a76d0ca3b983f4e91708
Raj Dahayat <dahayatraj775@gmail.com>
2026-07-20 23:36:49 +0530

    update

--- app/src/main/java/com/example/seva_knpc/MainActivity.java
+++ app/src/main/java/com/example/seva_knpc/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //noinspection deprecation
         getWindow().setNavigationBarColor(getResources().getColor(R.color.white));
         //noinspection deprecation
